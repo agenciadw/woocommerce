27bbecfea76944571d6b2c06c5158262bf7cbc2e
David William da Costa <50716862+agenciadw@users.noreply.github.com>
2022-05-14 08:15:20 -0300

    Update change-background-color-order.js

--- change-background-color-order.js
+++ change-background-color-order.js
@@ -1,8 +1,10 @@
+//Pago
+
 function change_order_processing_row_color(){?>
 
       <style type="text/css">
       .status-wc-processing {
-             background: #c5e9c5 !important;
+             background: #bbe1a0 !important;
              color: white !important;
             }
           </style>
@@ -11,12 +13,13 @@ function change_order_processing_row_color(){?>
      
         add_action('admin_head','change_order_processing_row_color');
      
-     
+//Aguardando
+
      function change_order_pending_row_color(){?>
      
       <style type="text/css">
       .status-wc-pending {
-             background: #ffc470 !important;
+             background: #ffc4707a !important;
              color: white !important;
             }
           </style>
@@ -25,3 +28,66 @@ function change_order_processing_row_color(){?>
      
         add_action('admin_head','change_order_pending_row_color');
      
+
+//Entregue
+
+function change_order_completed_row_color(){?>
+     
+      <style type="text/css">
+      .status-wc-completed {
+             background: #81d742 !important;
+             color: white !important;
+            }
+          </style>
+     
+        <?php }
+     
+        add_action('admin_head','change_order_completed_row_color');        
+
+
+//Cancelado
+
+function change_order_cancelled_row_color(){?>
+     
+      <style type="text/css">
+      .status-wc-cancelled {
+             background: #bfbfbf !important;
+             color: white !important;
+            }
+          </style>
+     
+        <?php }
+     
+        add_action('admin_head','change_order_cancelled_row_color');              
+
+
+//Pronto para entrega
+
+function change_order_driver_assigned_row_color(){?>
+     
+      <style type="text/css">
+      .status-wc-driver-assigned {
+             background: #7100e2 !important;
+             color: white !important;
+            }
+          </style>
+     
+        <?php }
+     
+        add_action('admin_head','change_order_driver_assigned_row_color');              
+        
+
+//Novo
+
+function change_order_novo_row_color(){?>
+     
+      <style type="text/css">
+      .status-wc-novo {
+             background: #ffc97999 !important;
+             color: white !important;
+            }
+          </style>
+     
+        <?php }
+     
+        add_action('admin_head','change_order_novo_row_color');             
